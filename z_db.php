9167e63c835e5a1e56ac8250a7606109abb110c2
riza <rizaali012@gmail.com>
2024-12-25 22:41:40 +0300

    All Files and Folder Added

---
+++ z_db.php
@@ -0,0 +1,26 @@
+<?php
+$con = new mysqli("localhost", "root", "", "vogue");
+if ($con->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+//Your Website URL Goes Here
+$url="http://localhost/vogue";
+
+
+//Your Admin Login URL Goes Here http://localhost/vogue/dashboard/login.php
+// Username: admin    Password: admin
+
+
+
+
+
+//Set Blog Activation Bonus Here (It must be only Number)
+$blog_bonus ="10";
+//Set Article Activation Bonus Here (It must be only Number)
+$art_bonus="10";
+//Set Daily Login Bonus Here (It must be only Number)
+$login_bonus="10";
+//Set Currency Symbol for daily login bonus Here
+$money="$";
+?>
